5fbf314a6bcb91bbaf605ffd9beb5b99544c3f6f
Pedro H Prado <pedrohenriquepradodenovaes@gmail.com>
2025-06-26 19:45:51 -0300

    Add files via upload

---
+++ JavaScript/aula6/index.js
@@ -0,0 +1,7 @@
+// Let ela permite declarar variáveis que podem ser alteradas posteriormente.
+let nome = 'João';
+
+console.log(nome);
+console.log("Olá, " + nome + "!");
+console.log(`Olá, ${nome}!`);
+console.log('Olá,', nome,  'tudo bem ?')
---
+++ JavaScript/aula7/index.js
@@ -0,0 +1,5 @@
+// const é uma variável que não pode ser reatribuída
+
+const nome = "João"
+console.log(nome);
+
